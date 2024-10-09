31de07a693f10e9ea4dd52093877812aaf94fddd
Alex Curnow <alex@curnows.com>
2024-10-09 14:56:39 +0000

    DEV: added url paths to category pages

--- blog/urls.py
+++ blog/urls.py
@@ -4,13 +4,9 @@ from django.urls import path
 urlpatterns = [
 # path('', views.HomePage.as_view(), name='home'),
 path('', views.PostList.as_view(), name='index'),
-path('<slug:slug>/', views.post_detail, name='post_detail'),
-    path(
-        '<slug:slug>/edit_comment/<int:comment_id>',
-        views.comment_edit, name='comment_edit'),
-    path(
-        '<slug:slug>/delete_comment/<int:comment_id>',
-        views.comment_delete, name='comment_delete'),
+path('post/<slug:slug>/', views.post_detail, name='post_detail'),
+path('post/<slug:slug>/edit_comment/<int:comment_id>', views.comment_edit, name='comment_edit'),
+path('post/<slug:slug>/delete_comment/<int:comment_id>', views.comment_delete, name='comment_delete'),
 path('backend/', views.backend_view, name='backend'),
 path('ai/', views.ai_view, name='ai'),
 path('cybersecurity/', views.cybersecurity_view, name='cybersecurity'),
