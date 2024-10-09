2b6f7a0ee01939ff1a232c752e5bcc4a273420ac
Chris Parry <chrisparry07@live.co.uk>
2024-10-09 13:01:18 +0000

    summernote added to main urls

--- Hackathon3/urls.py
+++ Hackathon3/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+path('accounts/', include('allauth.urls')),
 path('admin/', admin.site.urls),
 path('', include('blog.urls'), name='home'),
+path('summernote/', include('django_summernote.urls')),
 ]
